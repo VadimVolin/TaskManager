6f731e8548bb5f0324a24a3a44eb2f69c93f416f
VadimVolin <vadim.volin1@gmail.com>
2020-02-03 10:36:00 +0200

    CHANGE: do spinners not editable from keyboard

--- src/main/java/views/AddTaskView.java
+++ src/main/java/views/AddTaskView.java
@@ -40,7 +40,7 @@ public class AddTaskView extends JFrame {
                 container.remove(oneTimeTaskBtn);
                 container.remove(repeatTimeTaskBtn);
                 container.add(getRepeatTimeTaskPane());
-                currentFrame.setBounds(500,500, 400, 250);
+                currentFrame.setBounds(500,500, 400, 300);
             }
         });
 
@@ -53,6 +53,7 @@ public class AddTaskView extends JFrame {
         JLabel timeLabel = new JLabel("Task test");
         SpinnerDateModel dateModel = new SpinnerDateModel();
         JSpinner timeTask = new JSpinner(dateModel);
+        ((JSpinner.DefaultEditor)timeTask.getEditor()).getTextField().setEditable(false);
         jPanel.setLayout(new GridLayout(3, 2, 10, 10));
         jPanel.add(textLabel);
         jPanel.add(taskText);
@@ -71,28 +72,33 @@ public class AddTaskView extends JFrame {
         JLabel timestartLabel = new JLabel("Time start");
         SpinnerDateModel dateStartModel = new SpinnerDateModel();
         JSpinner timeStartTask = new JSpinner(dateStartModel);
+        ((JSpinner.DefaultEditor)timeStartTask.getEditor()).getTextField().setEditable(false);
 
         JLabel timeEndLabel = new JLabel("Time start");
         SpinnerDateModel dateEndModel = new SpinnerDateModel();
         JSpinner timeEndTask = new JSpinner(dateEndModel);
+        ((JSpinner.DefaultEditor)timeEndTask.getEditor()).getTextField().setEditable(false);
 
         JLabel intervalInMunutesLabel = new JLabel("Interval in munutes");
         SpinnerModel model = new SpinnerNumberModel(1, 1, 60, 1);
         JSpinner minutesInterval = new JSpinner(model);
         JComponent minutesEditor = new JSpinner.NumberEditor(minutesInterval);
         minutesInterval.setEditor(minutesEditor);
+        ((JSpinner.DefaultEditor)minutesInterval.getEditor()).getTextField().setEditable(false);
 
         JLabel intervalHoursLabel = new JLabel("Interval in hours");
         SpinnerModel hoursModel = new SpinnerNumberModel(0, 0, 24, 1);
         JSpinner hoursInterval = new JSpinner(hoursModel);
         JComponent hoursEditor = new JSpinner.NumberEditor(hoursInterval);
         hoursInterval.setEditor(hoursEditor);
+        ((JSpinner.DefaultEditor)hoursInterval.getEditor()).getTextField().setEditable(false);
 
         JLabel intervalDaysLabel = new JLabel("Interval in days");
         SpinnerModel daysModel = new SpinnerNumberModel(0, 0, 31, 1);
         JSpinner daysInterval = new JSpinner(daysModel);
         JComponent daysEditor = new JSpinner.NumberEditor(daysInterval);
         daysInterval.setEditor(daysEditor);
+        ((JSpinner.DefaultEditor)daysInterval.getEditor()).getTextField().setEditable(false);
 
         jPanel.setLayout(new GridLayout(7, 2, 10, 10));
         jPanel.add(textLabel);
